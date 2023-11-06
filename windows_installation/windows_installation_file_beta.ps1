5c57d0c82fc3ca2424281749c5e949407e729fb9
Rohith Ram A <ramvisha64@gmail.com>
2023-11-06 15:40:52 -0800

    Windows Installation script
    Installation script for windows using powershell to install both iverilog and extension

---
+++ windows_installation/windows_installation_file_beta.ps1
@@ -0,0 +1,25 @@
+﻿#code --install-extension verilog-runner.vsix
+
+#downloading the icarus verilog package from breyer.org
+$URL_iverilog = "https://bleyer.org/icarus/iverilog-0.9.7_setup.exe"
+$URL = "https://github.com/tridot64/verilog-runner/releases/download/Beta_vsix/verilog-runner-0.0.1.vsix"
+$path = Get-Location
+mkdir $path/src_win_verilog_install
+try {
+    Invoke-WebRequest -Uri $URL_iverilog -OutFile $path/src_win_verilog_install/install.exe
+    Invoke-WebRequest -Uri $URL -OutFile $path/src_win_verilog_install/verilog_run.vsix
+}
+catch {
+    <#Do this if a terminating exception happens#>
+    Write-Output "Error Occured Please run the script as administrator"
+}
+
+#Installing iverilog 0.9.7
+
+.\src_win_verilog_install\install.exe
+
+code --install-extension src_win_verilog_install/verilog_run.vsix
+
+
+
+
